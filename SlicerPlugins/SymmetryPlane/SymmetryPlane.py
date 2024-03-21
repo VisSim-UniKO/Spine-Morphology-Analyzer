e39b1de7c3bfef2f1c5deae3d636530e5f0ab7e5
Ivanna Kramer <ikramer@uni-koblenz.de>
2024-03-21 18:44:56 +0100

    add code and sawbone

--- SlicerPlugins/SymmetryPlane/SymmetryPlane.py
+++ SlicerPlugins/SymmetryPlane/SymmetryPlane.py
@@ -7,12 +7,18 @@ from slicer.ScriptedLoadableModule import *
 import numpy as np
 from scipy import stats
 from scipy.spatial import distance
+from scipy.optimize import least_squares
 import csv
 from pathlib import Path
 from vtk.util.numpy_support import vtk_to_numpy
+from vtk.util.numpy_support import numpy_to_vtk
+import open3d as o3d
 
+from scipy.linalg import lstsq
+from skspatial.objects import Plane, Points
 
 import SpineLib
+from  SpineLib import SlicerTools
 import vtk_convenience as conv
 
 #https://github.com/Caetox/SpineLib.git
@@ -54,7 +60,6 @@ class SymmetryPlaneWidget(ScriptedLoadableModuleWidget):
     def cleanup(self):
         pass
 
-    
 
 
 #
@@ -62,6 +67,10 @@ class SymmetryPlaneWidget(ScriptedLoadableModuleWidget):
 #
 
 class SymmetryPlaneLogic(ScriptedLoadableModuleLogic):
+
+    def __init__(self, pathToFiles):
+        self.vertebraFiles = pathToFiles
+        #if we need some parameters, set them here
 
 
     def updatePlane(self, vectors):
@@ -83,6 +92,75 @@ class SymmetryPlaneLogic(ScriptedLoadableModuleLogic):
 
         return angles
 
+    def calcInitalPlane(self, points, centerOfMass):
+        '''
+        points: nparray. Takes numpy array and calculates the pca axes to define the initial symmetry plane (sagittal plane in our case)
+        centerOfMass: tuple. It is used to display the axes
+        returns: inital sagittal plane
+        '''
+        lineNodeNames = ['e1', 'e2', 'e3']
+        lineNodes = []
+        lineStartPos = np.asarray(centerOfMass)
+        eigenvects = conv.pca_eigenvectors(points)
+        i = 0
+        planeNormal = eigenvects[0]
+
+        for lineNodeName in lineNodeNames:
+            e = eigenvects[i]
+            #we take e2 as normal to sagittal plane
+            lineEndPos = lineStartPos + (e*100)
+            lineNode = SlicerTools.markupsLineNode(lineNodeName, lineStartPos, lineEndPos)
+            lineNodes.append(lineNode)
+            i +=1 
+
+                    
+        # Get initial symmetry plane P0 using PCA
+        planeNode = slicer.mrmlScene.AddNewNodeByClass("vtkMRMLMarkupsPlaneNode", "initPlane_P0")
+        planeNode.SetPlaneType(slicer.vtkMRMLMarkupsPlaneNode.PlaneTypePointNormal)
+        print(centerOfMass)
+        planeNode.SetCenter(centerOfMass)
+        planeNode.SetNormal(planeNormal)
+
+        return planeNode
+
+
+    def cutVertebraModelWithPlane(self, planeNode, vertModelNode):
+        
+        planeCutTool = slicer.vtkSlicerDynamicModelerPlaneCutTool()
+          
+        outputModel_0 = slicer.mrmlScene.AddNewNodeByClass("vtkMRMLModelNode")
+        outputModel_1 = slicer.mrmlScene.AddNewNodeByClass("vtkMRMLModelNode")
+        
+        dynamicModelerNode = slicer.mrmlScene.AddNewNodeByClass("vtkMRMLDynamicModelerNode")
+        dynamicModelerNode.SetToolName("Plane cut")
+        dynamicModelerNode.SetNodeReferenceID("PlaneCut.InputModel", vertModelNode.GetID())
+        dynamicModelerNode.SetNodeReferenceID("PlaneCut.InputPlane", planeNode.GetID())
+        dynamicModelerNode.SetNodeReferenceID("PlaneCut.OutputPositiveModel", outputModel_0.GetID())
+        dynamicModelerNode.SetNodeReferenceID("PlaneCut.OutputNegativeModel", outputModel_1.GetID())
+        slicer.modules.dynamicmodeler.logic().RunDynamicModelerTool(dynamicModelerNode)
+
+        return outputModel_0, outputModel_1
+
+
+    def mirrorVertebraModelUponPlane(self, planeNode, vertModelNode, i):
+        print(f'Using plane node: {planeNode.GetName()}')
+        #we can use this output to print the attributes for this synamic modeler
+        #planeMirrorTool = slicer.vtkSlicerDynamicModelerMirrorTool()
+        
+        #for i in range(planeMirrorTool.GetNumberOfOutputNodes()):
+        #    print(planeMirrorTool.GetNthOutputNodeReferenceRole(i))
+
+        outputMirrored = slicer.mrmlScene.AddNewNodeByClass("vtkMRMLModelNode")
+        outputMirrored.SetName(f'Mirrored_{i}')
+        dynamicModelerNode = slicer.mrmlScene.AddNewNodeByClass("vtkMRMLDynamicModelerNode")
+        dynamicModelerNode.SetToolName("Mirror")
+        dynamicModelerNode.SetNodeReferenceID("Mirror.InputModel", vertModelNode.GetID())
+        dynamicModelerNode.SetNodeReferenceID("Mirror.InputPlane", planeNode.GetID())
+        dynamicModelerNode.SetNodeReferenceID("Mirror.OutputModel", outputMirrored.GetID())
+        slicer.modules.dynamicmodeler.logic().RunDynamicModelerTool(dynamicModelerNode)
+
+        return outputMirrored
+
 
     def loadModel(self, dirpath):
         for file in os.listdir(dirpath):
@@ -95,13 +173,137 @@ class SymmetryPlaneLogic(ScriptedLoadableModuleLogic):
                 slicer.util.loadTransform(filepath)
 
 
+    def getMiddlePoints(self, originalPoints, mirroredPoints, viz=True):
+        
+        if originalPoints.shape[0] > 10000:
+            inds = np.random.choice(originalPoints.shape[0], 10000)
+            originalPoints = np.take(originalPoints,inds, axis=0)
+            mirroredPoints = np.take(mirroredPoints,inds, axis=0)
+
+        middlepoints = (originalPoints + mirroredPoints)/2.0
+
+        if viz:
+
+            vtk_points = vtk.vtkPoints()
+            vtk_points.SetData(vtk.util.numpy_support.numpy_to_vtk(middlepoints))
+
+            # Create the vtkPolyData object.
+            polydata = vtk.vtkPolyData()
+            polydata.SetPoints(vtk_points)
+
+            # Create the vtkSphereSource object.
+            sphere = vtk.vtkSphereSource()
+            sphere.SetRadius(2.0)
+
+            # Create the vtkGlyph3D object.
+            glyph = vtk.vtkGlyph3D()
+            glyph.SetInputData(polydata)
+            glyph.SetSourceConnection(sphere.GetOutputPort())
+
+
+            pointCloudModelNode = slicer.modules.models.logic().AddModel(glyph.GetOutputPort())
+
+        return middlepoints
+
+
+        
+        
+    def fitPlaneLeastSquered(self, points, centerOfMass,i):
+        plane = Plane.best_fit(Points(points))
+        normal = plane.normal.round(3)
+        print(plane.normal.round(3))
+        print(type(normal))
+
+        # Get initial symmetry plane P0 using PCA
+        planeNode = slicer.mrmlScene.AddNewNodeByClass("vtkMRMLMarkupsPlaneNode", f"plane_P{i}")
+        planeNode.SetPlaneType(slicer.vtkMRMLMarkupsPlaneNode.PlaneTypePointNormal)
+        planeNode.SetCenter(centerOfMass)
+        planeNode.SetNormal(normal)
+
+        return planeNode
+
+
+    def fitPlaneSVD(self, points, centerOfMass, i):
+
+        A = np.c_[points[:, 0:2], np.ones(points.shape[0])]
+        print(A.shape)
+        b = points[:, 0]
+        print(b.shape)
+        
+        p, res, rnk, s = lstsq(A, b)
+        print(p, res, rnk, s )
+        print("Plane coefficients [A, B, C]:", p)
+
+        # Get initial symmetry plane P0 using PCA
+        planeNode = slicer.mrmlScene.AddNewNodeByClass("vtkMRMLMarkupsPlaneNode", f"plane_P{i}")
+        planeNode.SetPlaneType(slicer.vtkMRMLMarkupsPlaneNode.PlaneTypePointNormal)
+        planeNode.SetCenter(centerOfMass)
+        planeNode.SetNormal(p)
+
+        return planeNode
+
+
+
+    def evaluateRegistrationICP(self, originalPoints, mirroredPoints):
+        source = o3d.geometry.PointCloud()
+        source.points = o3d.utility.Vector3dVector(originalPoints)
+        target = o3d.geometry.PointCloud()
+        target.points = o3d.utility.Vector3dVector(mirroredPoints)
+        threshold = 10000
+        evaluation = o3d.pipelines.registration.evaluate_registration(source, target, threshold)
+
+        print(f'evaluation.fitness={evaluation.fitness}, evaluation.inlier_rmse={evaluation.inlier_rmse}')
+        return evaluation.fitness, evaluation.inlier_rmse
+
+
+    def registerWithVanilaICP(self, originalPoints, mirroredPoints):
+        threshold = 100000 #mm
+        trans_init = np.identity(4)
+        source = o3d.geometry.PointCloud()
+        source.points = o3d.utility.Vector3dVector(originalPoints)
+        target = o3d.geometry.PointCloud()
+        target.points = o3d.utility.Vector3dVector(mirroredPoints)
+        reg_p2p = o3d.pipelines.registration.registration_icp(source, target, threshold, trans_init, o3d.pipelines.registration.TransformationEstimationPointToPoint())
+        icp = reg_p2p.transformation
+        transformationMatrix = vtk.vtkMatrix4x4()
+
+        # Populate the VTK matrix with values from the NumPy matrix
+        for i in range(4):
+            for j in range(4):
+                transformationMatrix.SetElement(i, j, icp[i, j])
+
+        return transformationMatrix
+
+
+
     # def run(self, directory, clAngle, tkAngle, llAngle, cAngle, ivdHeight):
     def run(self):
         print('Running Logic')
-
-    
-
-            
+        sawboneModelNodes = []
+        transformationMatrix = None
+        for file in self.vertebraFiles:
+            if file.endswith(".stl") or file.endswith(".obj"):
+                vertebraModelNode = slicer.util.loadModel(file)
+                sawboneModelNodes.append(vertebraModelNode)
+                polydata = vertebraModelNode.GetPolyData()
+                centerOfMass = conv.calc_center_of_mass(polydata)
+                points = SlicerTools.pointsFromModelNode_asNumPy(vertebraModelNode)
+                
+                p0 = self.calcInitalPlane(points, centerOfMass)
+                mirroredVertNode = self.mirrorVertebraModelUponPlane(p0, vertebraModelNode, 100)
+                for i in range(5):
+                
+                    mirroredModelPoints = SlicerTools.pointsFromModelNode_asNumPy(mirroredVertNode)
+                    transformationMatrix = self.registerWithVanilaICP(mirroredModelPoints, points)
+                    SpineLib.SlicerTools.transformOneObject(transformationMatrix, mirroredVertNode)
+                    mirroredModelPoints = SlicerTools.pointsFromModelNode_asNumPy(mirroredVertNode)
+                    self.evaluateRegistrationICP(mirroredModelPoints, points)
+                    
+                    middlePoints = self.getMiddlePoints(points, mirroredModelPoints)
+                    p_i = self.fitPlaneLeastSquered(middlePoints, centerOfMass, i)
+                    mirroredVertNode = self.mirrorVertebraModelUponPlane(p_i, vertebraModelNode, i)
+                           
+                
 
 
 class SymmetryPlaneTest(ScriptedLoadableModuleTest):
@@ -123,8 +325,7 @@ class SymmetryPlaneTest(ScriptedLoadableModuleTest):
         self.test_SymmetryPlane1()
 
     def test_SymmetryPlane1(self):
-        symPlaneLogic = SymmetryPlaneLogic()
-        symPlaneLogic.run()
+        
 
         self.delayDisplay("Starting the test")
         # load segmentation models
@@ -134,38 +335,8 @@ class SymmetryPlaneTest(ScriptedLoadableModuleTest):
         print(f'{parentFolder}')
         sawboneDirectory = os.path.join(parentFolder, "L1")
         sawboneFiles = slicer.util.getFilesInDirectory(sawboneDirectory)
-        sawboneModelNodes = []
-
-        for file in sawboneFiles:
-            if file.endswith(".stl") or file.endswith(".obj"):
-                node = slicer.util.loadModel(file)
-                sawboneModelNodes.append(node)
-                polydata = node.GetPolyData()
-                centerOfMass = conv.calc_center_of_mass(polydata)
-                points = polydata.GetPoints()
-                array = points.GetData()
-                points = vtk_to_numpy(array)
-                lineNodeNames = ['e1', 'e2', 'e3']
-                lineStartPos = np.asarray(centerOfMass)
-                # Get initial symmetry plane P0 using PCA
-                eigenvects = conv.pca_eigenvectors(points)
-                i = 0
-                for lineNodeName in lineNodeNames:
-                    e = eigenvects[i]
-                    print(e)
-                    lineNode = slicer.mrmlScene.AddNewNodeByClass("vtkMRMLMarkupsLineNode", lineNodeName)
-                    
-                    print(f'lineStartPos: {lineStartPos}')
-                    lineEndPos = lineStartPos + (e*100)
-                    print(f'lineEndPos: {lineEndPos}')
-                    lineNode.AddControlPointWorld(lineStartPos, f'start{i}')
-                    lineNode.AddControlPointWorld(lineEndPos, f'end{i}')
-                    i+=1
-                    #lineDirectionVector = (lineEndPos-lineStartPos)/np.linalg.norm(lineEndPos-lineStartPos)
-                    
-
-                # Compute the covariance matrix and principal components without centering
-
-
+        
+        symPlaneLogic = SymmetryPlaneLogic(sawboneFiles)
+        symPlaneLogic.run()
 
         self.delayDisplay('Test passed!')
